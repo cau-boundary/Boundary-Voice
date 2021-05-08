afe5047cfd7f3b5bda772d713d824692d7cd4ca3
WOODI <woodi.daily@gmail.com>
2021-05-08 17:41:56 +0900

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withPlugins = require("next-compose-plugins");
+const withSass = require("@zeit/next-sass");
+const withImages = require("next-images");
+module.exports = withPlugins([
+	[
+		withSass,
+		{
+			cssModules: true,
+			cssLoaderOptions: {
+				importLoaders: 1,
+				localIdentName: "[local]___[hash:base64:5]",
+			},
+		},
+	],
+	[withImages()],
+]);
