fe1020e85e738096a1991fa6f57e4a7cb5da15d9
WOODI <woodi.daily@gmail.com>
2021-05-19 16:35:48 +0900

    solve lte connection problem

--- src/components/webRTC/index.tsx
+++ src/components/webRTC/index.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { usePeer, useRemoteStream, useUserMedia } from "@src/hooks";
 
+import Peer from "peerjs";
 import RTCMedia from "@src/components/webRTC/rtcMedia";
 import { SocketContext } from "@src/util/websocket";
 import styles from "@src/components/webRTC/index.module.scss";
@@ -18,7 +19,7 @@ const config = { iceServers: [{ urls: ["stun:stun.l.google.com:19302"] }] };
 
 const localConfig = {
 	host: "/",
-	port: 443,
+	port: 3000,
 	path: "/media-chat",
 	config,
 	// secure: true,
@@ -29,6 +30,7 @@ export default function VideoRoom(props: IVideoRoomProps) {
 	const { roomId } = props;
 	const socket = useContext(SocketContext);
 	const peers = {};
+	const [myPeer, setPeer] = useState<Peer>(undefined);
 	const localStream = useUserMedia();
 	const [
 		remoteStreams,
@@ -37,17 +39,28 @@ export default function VideoRoom(props: IVideoRoomProps) {
 	] = useRemoteStream();
 	// const [myPeer, myPeerID] = usePeer(addRemoteStream, removeRemoteStream,roomId);
 
+	const cleanUp = () => {
+		if (myPeer) {
+			myPeer.disconnect();
+			myPeer.destroy();
+		}
+		setPeer(null);
+	};
+
 	useEffect(() => {
 		if (localStream) {
 			import("peerjs").then(({ default: Peer }) => {
 				const myPeer = new Peer(undefined, localConfig);
+				setPeer(myPeer);
 
 				myPeer.on("open", (id) => {
 					socket.emit("join-room", roomId, id);
 				});
 
 				myPeer.on("call", (call) => {
+					console.log("receiving call from" + call.peer);
 					call.answer(localStream);
+
 					call.on("stream", (remoteStream) => {
 						addRemoteStream(remoteStream, call.peer);
 					});
@@ -63,24 +76,22 @@ export default function VideoRoom(props: IVideoRoomProps) {
 					});
 				});
 
-				myPeer.on("disconnected", () => {
-					console.log("Peer disconnected");
-					myPeer.disconnect();
-					myPeer.destroy();
-				});
-
 				myPeer.on("close", () => {
 					console.log("Peer closed remotetly");
-					myPeer.disconnect();
-					myPeer.destroy();
+					cleanUp();
+				});
+
+				myPeer.on("disconnected", () => {
+					console.log("Peer disconnected");
+					cleanUp();
 				});
 
 				myPeer.on("error", (error) => {
 					console.log("peer error", error);
-					myPeer.disconnect();
-					myPeer.destroy();
+					cleanUp();
 				});
 
+				// caller section
 				socket.on("user-connected", (userID) => {
 					//call.peer == userID
 					const call = myPeer.call(userID, localStream);
@@ -88,6 +99,17 @@ export default function VideoRoom(props: IVideoRoomProps) {
 					// add other users media stream
 					call.on("stream", (remoteStream) => {
 						addRemoteStream(remoteStream, call.peer);
+						console.log("connected to" + call.peer);
+					});
+					call.on("close", () => {
+						console.log("call closed");
+						removeRemoteStream(call.peer);
+						call.close();
+					});
+					call.on("error", (error) => {
+						console.log("call error", error);
+						removeRemoteStream(call.peer);
+						call.close();
 					});
 					peers[userID] = call;
 				});
@@ -99,11 +121,19 @@ export default function VideoRoom(props: IVideoRoomProps) {
 				});
 			});
 		}
+		return () => {
+			cleanUp();
+		};
 	}, [localStream]);
 
 	const onButtonClick = (e) => {
 		e.preventDefault();
-		console.log(e);
+		if (window.confirm("Disconnect from users")) {
+			myPeer.disconnect();
+			myPeer.destroy();
+		} else {
+			window.alert("cancelled");
+		}
 	};
 
 	return (
